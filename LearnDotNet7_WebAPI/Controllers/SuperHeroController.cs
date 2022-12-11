c20227165fdd8cbeb896c687e743830334e48b05
phanphuc2210 <phucpth2001@gmail.com>
2022-12-11 13:05:40 +0700

    Add a new Controller

---
+++ LearnDotNet7_WebAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnDotNet7_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+    }
+}
